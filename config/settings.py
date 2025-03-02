7b3a1bbd2846f8ec83bd1871da4aa4e8f63cb04f
Lysenko Nikita <nike7cr77@ya.ru>
2025-03-02 18:39:53 +0700

    pypline_30

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "db",
-        "PORT": os.getenv("PORT"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
